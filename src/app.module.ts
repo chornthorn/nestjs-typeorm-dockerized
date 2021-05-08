34d79c5149eff086d98b1ca8449c4992bed73684
Chorn Thorn <chornthorn.kh@gmail.com>
2021-05-09 04:38:10 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
